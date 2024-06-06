24d6b7f039a6903b9c32e53bc6546e3bbd51b2a3
Jan romel <janjancaranguian@gmail.com>
2024-06-06 15:32:52 +0800

    First commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+  echo"Hello world <br>";
+  echo"Goodbye world";
+  // This is a comment
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html>
