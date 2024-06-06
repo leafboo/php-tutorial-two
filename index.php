de509d556df3c1e530839670a59840ab6524a946
Jan romel <janjancaranguian@gmail.com>
2024-06-06 16:21:52 +0800

    Practice variables in php

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
 <?php
-  echo"Hello world <br>";
-  echo"Goodbye world";
+  $name = "Jan Romel";
+  echo "Hello {$name} <br>";
+
+  $age = 21;
+  echo "You are $age <br>";
+
+  $bank = 69;
+  echo "Your balance is: \$" . ($age + $bank);
   // This is a comment
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  
-</body>
-</html>
