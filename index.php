a5412b114bb901d1548355cfc564c3f2784da64b
Jan romel <janjancaranguian@gmail.com>
2024-06-07 16:46:33 +0800

    Associative array

--- index.php
+++ index.php
@@ -2,18 +2,22 @@
  
 <?php
   
-  
-  $foods = array("apple", "banana", "orange", "grapes");
-  //array_push($foods, "pineapple", "kiwi");
-  //array_pop($foods);
-  //array_shift($foods);
-  $foods = array_reverse($foods);
+  // associative arrays are just dictionaries in python and objects in js
 
-  foreach($foods as $food){
-    echo $food . "<br>";
+  $capitals = array(
+                      "USA" => "Washington D.C.",
+                      "Japan" => "Tokyo",
+                      "South Korea" => "Seoul",
+                      "India" => "New Delhi"
+  );
+  // $keys = array_keys($capitals);
+  //$values = array_values($capitals);
+  //$capitals = array_flip($capitals);
+  //$capitals = array_reverse($capitals);
+
+  foreach($capitals as $key => $value) {
+    echo "{$key} -> {$value}<br>";
   }
-
-  echo count($foods)
   
 
 ?>
