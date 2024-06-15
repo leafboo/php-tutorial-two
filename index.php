87de240e7cc33825472a12a3b1b07727841b9438
Jan <110758056+leafboo@users.noreply.github.com>
2024-06-15 14:32:07 +0800

    Sanitize

--- index.php
+++ index.php
@@ -1,15 +1,32 @@
-<?php
-  // isset() = Returns TRUE if a variable is declared and not null
-  // empty() = Returns TRUE if a variable is not declared, false, null, ""
-  // Note: $_POST is an associative array
-  
-  $username = "hello";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="index.php" method="post">
+    username: <br>
+    <input type="text" name="username"><br>
+    age: <br>
+    <input type="text" name="age"><br>
+    email: <br>
+    <input type="text" name="email">
+    <input type="submit" name="login" id="login">
+  </form>
+</body>
+</html>
 
-  if (empty($username)){
-    echo "This variable is empty";
-  } else {
-    echo "This variable is NOT empty";
+<?php
+  if (isset($_POST["login"])) {
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+    $age = filter_input(INPUT_POST, "age", FILTER_SANITIZE_NUMBER_INT);
+    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+
+    echo "Your email is {$email}";
   }
+  
 
 ?>
 
