d435ee39c7d657e063ab2abf926ac2867a3a3e70
Jan <110758056+leafboo@users.noreply.github.com>
2024-06-16 17:55:07 +0800

    Hashing (kinda like encryption?)

--- start.php
+++ start.php
@@ -1,25 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
-    username <br>
-    <input type="text" name="username">
-    <input type="submit" name="submit">
-  </form>
-</body>
-</html>
-
 <?php
-  foreach($_SERVER as $key => $value) {
-    echo "{$key} = {$value} <br>";
-  }
+  $password = "pizza123";
 
-  if ($_SERVER["REQUEST_METHOD"] == "POST") { // more reliable way of checking if the submit button is pressed
-    echo "Hello";
+  $hash = password_hash($password, PASSWORD_DEFAULT);
+
+  if (password_verify("pizza123", $hash)) {
+    echo "access granted";
+  } else {
+    echo "access denied";
   }
 ?>
