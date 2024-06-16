342edf2aba64c2513e2dc6833df6d6cdf691357d
Jan <110758056+leafboo@users.noreply.github.com>
2024-06-16 14:15:10 +0800

    _SESSION

--- index.php
+++ index.php
@@ -1,18 +1,38 @@
 <?php
-  setcookie("fav_food", "pizza", time() + (86400 * 2), "/");
-  setcookie("fav_drink", "coffee", time() + (86400 * 2), "/");
-  setcookie("fav_dessert", "ice cream", time() + (86400 * 2), "/");
+  // session - used to store information like user account to be used 
+  // accross multiple pages of that site
 
-  /*
-    foreach($_COOKIE as $key => $value) {
-      echo "{$key} = {$value} <br>";
+  session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="index.php" method="post">
+    username: <br>
+    <input type="text" name="username"> <br> 
+    password: <br>
+    <input type="password" name="password"> <br> 
+    <input type="submit" name="login" value="login">
+
+  </form>
+</body>
+</html>
+
+<?php
+  if (isset($_POST["username"])) {
+    if (!empty($_POST["username"]) && !empty($_POST["password"])) {
+      $_SESSION["username"] = $_POST["username"];
+      $_SESSION["password"] = $_POST["password"];
+      header("Location: home.php");
+    } else {
+      echo "Missing username/ password <br>";
     }
-  */
-  
-
-  if (isset($_COOKIE["fav_food"])) {
-    echo "BUY SOME {$_COOKIE["fav_food"]} !!!";
-  } else {
-    echo "I don't know your favorite food";
   }
+  
 ?>
