949ad14a9fcd008078c9a77fb95edca570bcbbbf
Jan Romel <janjancaranguian@gmail.com>
2024-06-22 17:15:59 +0800

    Make a form that registers user account to the database

--- index.php
+++ index.php
@@ -1,21 +1,48 @@
 <?php
   include("database.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post" >
+    <h2>Welcome to Fakebook!</h2>
+    username: <br>
+    <input type="text" name="username"> <br>
+    password: <br>
+    <input type="password" name="password"> <br>
+    <input type="submit" name="submit" value="register">
+  </form>
 
-  $sql = "SELECT * FROM users WHERE user = 'Spongebob'";
-  $result = mysqli_query($conn, $sql);
+</body>
+</html>
 
-  //while ($row = mysqli_fetch_assoc($result)) {
-    //echo $row["id"] . "<br>";
-    //echo $row["user"] . "<br>";
-    //echo $row["reg_date"] . "<br>";
-  //}
-  if (mysqli_num_rows($result) > 0) {
-    $row = mysqli_fetch_assoc($result);
-    echo $row["id"] . "<br>";
-    echo $row["user"] . "<br>";
-    echo $row["reg_date"] . "<br>";
-  } else {
-    echo "No user found";
+<?php
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+
+    if(empty($username)) {
+      echo "Please enter a username";
+    } else if(empty($password)) {
+      echo "Please enter a password";
+    } else {
+      $hash = password_hash($password, PASSWORD_DEFAULT);
+      $sql = "INSERT INTO users (user, password)
+              VALUES ('$username', '$hash')";
+      try {
+        mysqli_query($conn, $sql);
+        echo "You are now registered! congratulations bud";
+      } catch(mysqli_sql_exception) {
+        echo "That username is already taken";
+      }
+      
+    }
+
   }
 
   mysqli_close($conn);
